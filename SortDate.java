ccdae7a160e58dcd21b742c8499f0a43fcacd9d7
MsParvika <43131186+MsParvika@users.noreply.github.com>
2019-03-07 17:25:02 -0700

    Create SortDate.java
    Sorts the date in java using CompareTo method

---
+++ SortDate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class SortDate {
+
+    public static List<String> sortDates(List<String> dates) {
+        java.time.format.DateTimeFormatter dateTimeFormatter = java.time.format.DateTimeFormatter.ofPattern("dd MMM yyyy");
+        Collections.sort(dates,
+                (x, y) -> java.time.LocalDate.parse(x, dateTimeFormatter).compareTo(java.time.LocalDate.parse(y, dateTimeFormatter)));
+        return dates;
+    }
+
+    public static void main(String arg[]) {
+        Schedule schedule = new Schedule();
+        List<String> dates = new ArrayList<String>(Arrays.asList("26 Jan 1992","31 Dec 1883","12 Jun 2018","25 Mar 2000"));
+        List<String> result = schedule.sortDates(dates);
+        for(String r : result){
+            System.out.println(r);
+        }
+    }
+}
